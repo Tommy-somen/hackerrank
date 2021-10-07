ae04fed8cbbdc62248bf0471ff9e62b7c15b33f3
Tomohiro Wakamatsu <66423095+Tommy-somen@users.noreply.github.com>
2021-10-07 14:48:01 +0900

    Create Day 1: Data Types.py

---
+++ 30Days_of_Code/Day 1: Data Types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+secound_int = int(input())
+secound_float = float(input())
+secound_str = str(input()) 
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+secound_int)
+# Print the sum of the double variables on a new line.
+print(d+secound_float)
+# Concatenate and print the String variables on a new line
+print(s+secound_str)
+# The 's' variable above should be printed first.
